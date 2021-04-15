c1222cbb71c988fb470bc34b485b415261149189
yyyyyyyan <contact@yyyyyyyan.tech>
2021-04-14 21:03:09 -0300

    Create from_address classmethod.

--- onesecmail/mail.py
+++ onesecmail/mail.py
@@ -37,6 +37,11 @@ class OneSecMail:
         return f"{self.user}@{self.domain}"
 
     @classmethod
+    def from_address(cls, address, **requests_kwargs):
+        user, domain = address.split("@")
+        return cls(user, domain, **requests_kwargs)
+
+    @classmethod
     def get_random_mailbox(cls, **requests_kwargs):
         response = requests.get(
             cls.API_URL, params={"action": "genRandomMailbox"}, **requests_kwargs
