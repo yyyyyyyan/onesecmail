fbe8eb3b89cf0705abd30cac40198719f99f11e9
yyyyyyyan <contact@yyyyyyyan.tech>
2021-04-14 21:37:26 -0300

    Add __version__.py

--- onesecmail/__init__.py
+++ onesecmail/__init__.py
@@ -1,1 +1,9 @@
+from onesecmail.__version__ import __author__  # noqa: F401
+from onesecmail.__version__ import __author_email__  # noqa: F401
+from onesecmail.__version__ import __copyright__  # noqa: F401
+from onesecmail.__version__ import __description__  # noqa: F401
+from onesecmail.__version__ import __license__  # noqa: F401
+from onesecmail.__version__ import __title__  # noqa: F401
+from onesecmail.__version__ import __url__  # noqa: F401
+from onesecmail.__version__ import __version__  # noqa: F401
 from onesecmail.mail import OneSecMail  # noqa: F401
---
+++ onesecmail/__version__.py
@@ -0,0 +1,15 @@
+#                                                   _ _
+#    ___  _ __   ___  ___  ___  ___ _ __ ___   __ _(_) |
+#   / _ \| '_ \ / _ \/ __|/ _ \/ __| '_ ` _ \ / _` | | |
+#  | (_) | | | |  __/\__ \  __/ (__| | | | | | (_| | | |
+#   \___/|_| |_|\___||___/\___|\___|_| |_| |_|\__,_|_|_|
+#
+
+__title__ = "onesecmail"
+__description__ = "The pythonic temporary mailbox!"
+__url__ = "https://github.com/yyyyyyyan/onesecmail"
+__version__ = "0.0.1"
+__author__ = "yyyyyyyan"
+__author_email__ = "contact@yyyyyyyan.tech"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2021 yyyyyyyan"
