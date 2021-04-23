ec25f3224e4cfa314e5dcd1774ca872faa62b4af
yyyyyyyan <contact@yyyyyyyan.tech>
2021-04-22 22:09:20 -0300

    Turn id, from_address and date into non-settable properties.

--- onesecmail/message.py
+++ onesecmail/message.py
@@ -14,15 +14,27 @@ class EmailMessage:
         attachments=None,
         date_offset="+0000",
     ):
-        self.id = message_id
-        self.from_address = from_address
+        self._id = message_id
+        self._from_address = from_address
         date += date_offset
-        self.date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S%z")
+        self._date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S%z")
         self.subject = subject
         self.body = body
         self.text_body = text_body
         self.html_body = html_body
         self.attachments = attachments or []
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def from_address(self):
+        return self._from_address
+
+    @property
+    def date(self):
+        return self._date
 
     @classmethod
     def from_dict(cls, data, **kwargs):
