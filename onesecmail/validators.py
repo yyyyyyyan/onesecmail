8aff61d8e3af750b68f3923a97cb849de72fbfc0
yyyyyyyan <contact@yyyyyyyan.tech>
2021-05-13 21:38:36 -0300

    Add DateRangeValidator.

--- onesecmail/validators.py
+++ onesecmail/validators.py
@@ -1,4 +1,6 @@
 import re
+from datetime import datetime
+from datetime import timezone
 
 
 class RegexValidator:
@@ -17,3 +19,22 @@ class FromAddressValidator(RegexValidator):
 class SubjectValidator(RegexValidator):
     def __call__(self, message):
         return super().__call__(message.subject)
+
+
+class DateRangeValidator:
+    def __init__(self, min_date=None, max_date=None):
+        if min_date is None:
+            self.min_date = datetime.min.replace(tzinfo=timezone.utc)
+        else:
+            self.min_date = (
+                min_date.astimezone() if min_date.tzinfo is None else min_date
+            )
+        if max_date is None:
+            self.max_date = datetime.max.replace(tzinfo=timezone.utc)
+        else:
+            self.max_date = (
+                max_date.astimezone() if max_date.tzinfo is None else max_date
+            )
+
+    def __call__(self, message):
+        return self.min_date < message.date < self.max_date
