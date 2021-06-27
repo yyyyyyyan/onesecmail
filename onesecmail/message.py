e9422a86eb39817aeb44e1211b60c53ec964ba55
yyyyyyyan <contact@yyyyyyyan.tech>
2021-06-27 19:07:50 -0300

    Document EmailMessage class and onesecmail.message module.

--- onesecmail/message.py
+++ onesecmail/message.py
@@ -1,7 +1,45 @@
+"""This module contains the `EmailMessage` class definition."""
 from datetime import datetime
 
 
 class EmailMessage:
+    """Represents a 1secmail message.
+
+    Parameters
+    ----------
+    message_id : int
+    from_address : str
+    to_address : str
+    date : str
+        The date should follow the strftime format "%Y-%m-%d %H:%M:%S".
+    subject : str, optional
+    body : str, optional
+    text_body : str, optional
+    html_body : str, optional
+    attachments : list, optional
+    mail_handler : onesecmail.OneSecMail, optional
+        The 1secmail mailbox to use to fetch the message's content or download attachments.
+
+    Attributes
+    ----------
+    DATE_OFFSET : str, default: "+0200"
+        The message's date UTC offset ('%z').
+    id : int
+    from_address : str
+    to_address : str
+    date : datetime.datetime
+    subject : str
+        The message's subject.
+    body : str
+        The message's body in any format.
+    html_body : str
+        The message's HTML body.
+    text_body : str
+        The message's text body.
+    attachments : list of dict
+        The message's list of attachments, without their content.
+    """
+
     DATE_OFFSET = "+0200"
 
     def __init__(
@@ -35,22 +73,36 @@ class EmailMessage:
 
     @property
     def id(self):
+        """The message ID. This value should not be changed."""
         return self._id
 
     @property
     def from_address(self):
+        """The sender's email address. This value should not be changed."""
         return self._from_address
 
     @property
     def to_address(self):
+        """The recipient's email address. This value should not be changed."""
         return self._to_address
 
     @property
     def date(self):
+        """The message's date. This value should not be changed."""
         return self._date
 
     @classmethod
     def from_dict(cls, data, **kwargs):
+        """Creates an EmailMessage from a message dictionary.
+
+        Parameters
+        ----------
+        data : dict
+            The message dictionary. Used keys are 'id', 'from', 'to',
+            'subject', 'date', 'attachments', 'body', 'textBody' and 'htmlBody'.
+        **kwargs : dict, optional
+            Additional kwargs will be passed into the EmailMessage constructor.
+        """
         message_data = {
             "message_id": data["id"],
             "from_address": data["from"],
@@ -65,6 +117,26 @@ class EmailMessage:
         return cls(**message_data, **kwargs)
 
     def fetch_content(self, message_data=None):
+        """Fetchs the message data.
+
+        Fetchs the email message content (subject,
+        body, textBody, htmlBody and attachments) into the
+        EmailMessage object.
+
+        Parameters
+        ----------
+        message_data : dict, optional
+            A dictionary containing the message data to be incorporated
+            by the EmailMessage object. If None is provided, the message
+            content will be fetch using the `OneSecMail.get_message_as_dict()`
+            method.
+
+        Raises
+        ------
+        NotImplementedError
+            If message_data is None and the EmailMessages' mail_handler
+            is not set.
+        """
         if message_data is None:
             if self._mail_handler is None:
                 raise NotImplementedError(
